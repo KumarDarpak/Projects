f4dad710e3fd330fb5d23dd9bab4153e5af613b3
KumarDarpak <kumardarpak@gmail.com>
2026-06-02 23:13:03 +0530

    updated the whole numbers

--- Smartcalculator/script.js
+++ Smartcalculator/script.js
@@ -6,3 +6,15 @@ const c_classBttn = document.querySelector(".c_class");
 c_classBttn.addEventListener("click", () => {
   header.innerText = "Cleared the Screen";
 });
+
+const bksp_classBttn = document.querySelector(".bksp_class");
+
+bksp_classBttn.addEventListener("click", () => {
+  if (header.innerText.length > 0) {
+    header.innerText = header.innerText.slice(0, -1);
+  }
+});
+
+const numButtons = document.querySelectorAll(
+  ".num_0, .num_1, .num_2, .num_3, .num_4, .num_5, .num_6, .num_7, .num_8, .num_9",
+);
